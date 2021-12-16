5de5d160d83c834097059c2be3af0c0598d3629c
Wojciech Kępka <wojciech@wkepka.dev>
2021-12-16 18:36:19 +0100

    Add `Container::start` method
    Closes: #21

--- src/api/containers.rs
+++ src/api/containers.rs
@@ -1,10 +1,33 @@
-use crate::{models, opts::ContainerListOpts, util::url, Result};
+use crate::{conn::Payload, models, opts::ContainerListOpts, util::url, Result};
 
 impl_api_ty!(
     Container => id
 );
 
-impl<'podman> Container<'podman> {}
+impl<'podman> Container<'podman> {
+    api_doc! {
+    Container => StartLibpod
+    /// Start this container.
+    ///
+    /// Parameters:
+    ///  * detach_keys - Override the key sequence for detaching a container. Format is a single
+    ///                  character [a-Z] or ctrl- where is one of: a-z, @, ^, [, , or _.
+    ///
+    /// Examples:
+    ///
+    /// ```no_run
+    /// let podman = Podman::unix("/run/user/1000/podman/podman.sock");
+    ///
+    /// if let Err(e) = podman.containers().get("79c93f220e3e").start(None).await {
+    ///     eprintln!("{}", e);
+    /// }
+    /// ```
+    |
+    pub async fn start(&self, detach_keys: Option<String>) -> Result<()> {
+        let ep = url::construct_ep(&format!("/libpod/containers/{}/start", &self.id), detach_keys.map(|d| url::encoded_pair("detachKeys", d)));
+        self.podman.post(&ep, Payload::None::<&str>).await.map(|_| ())
+    }}
+}
 
 impl<'podman> Containers<'podman> {
     api_doc! {
@@ -15,6 +38,7 @@ impl<'podman> Containers<'podman> {
     ///
     /// ```no_run
     /// let podman = Podman::unix("/run/user/1000/podman/podman.sock");
+    ///
     /// for container in podman
     ///     .containers()
     ///     .list(
